0dbb94b634c847b20577543a072bdb893d93066b
Krish Patel <krishp038@gmail.com>
2023-12-03 20:03:45 +0000

    fix

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,18 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 4, 'cols': 15}),
+        max_length=120,
+        required=False
+    )
+
+    quantity = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'min': 0, 'max': 50}),
+        validators=[MinValueValidator(0), MaxValueValidator(50)]
+    )
