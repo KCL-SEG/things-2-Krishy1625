65582d388e375641502575d717ce5161c7b6b1c8
Krish Patel <krishp038@gmail.com>
2023-12-03 20:12:46 +0000

    complete task

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from things import views
+from things.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', home, name='home'),
 ]
--- things/views.py
+++ things/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from .models import Thing
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            return redirect('home')  
+    else:
+        form = ThingForm()
+
+    things = Thing.objects.all() 
+
+    return render(request, 'home.html', {'form': form, 'things': things})
